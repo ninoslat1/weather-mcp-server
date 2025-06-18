456595022b469c65fe388bb133ee8b98e5a44829
ninoslat1 <benedict.ellnina@gmail.com>
2025-06-18 16:57:44 +0700

    Add project files.

---
+++ MCP/Extensions/MCPHttpClient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace WeatherMCP.MCP.Extensions
+{
+    internal static class MCPHttpClient
+    {
+        public static async Task<JsonDocument> ReadJsonDoc(this HttpClient httpClient, string reqUri)
+        {
+            using var response = await httpClient.GetAsync(reqUri);
+            response.EnsureSuccessStatusCode();
+            return await JsonDocument.ParseAsync(await response.Content.ReadAsStreamAsync());
+        }
+    }
+}
---
+++ Models/Weather.cs
@@ -0,0 +1,20 @@
+﻿using ModelContextProtocol.Server;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherMCP.Models
+{
+    [McpServerToolType]
+    public static class Weather
+    {
+        [McpServerTool, Description("Get weather alerts for INA state")]
+        public static async Task<string> GetAlerts()
+        {
+
+        }
+    }
+}
