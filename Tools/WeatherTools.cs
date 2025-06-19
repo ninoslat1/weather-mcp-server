d9c054449fb0f5daa50030afd7688d3b1bd3c849
ninoslat1 <benedict.ellnina@gmail.com>
2025-06-19 08:09:30 +0700

    feat: add guard for api calls

--- Tools/WeatherTools.cs
+++ Tools/WeatherTools.cs
@@ -19,6 +19,26 @@ namespace WeatherMCP.Tools
             [Description("The INA state to get alerts for")] string state
         )
         {
+            var IndonesianStates = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+            {
+                "Aceh", "Bali", "Banten", "Bengkulu", "DI Yogyakarta", "DKI Jakarta",
+                "Gorontalo", "Jambi", "Jawa Barat", "Jawa Tengah", "Jawa Timur",
+                "Kalimantan Barat", "Kalimantan Selatan", "Kalimantan Tengah",
+                "Kalimantan Timur", "Kalimantan Utara", "Kepulauan Bangka Belitung",
+                "Kepulauan Riau", "Lampung", "Maluku", "Maluku Utara",
+                "Nusa Tenggara Barat", "Nusa Tenggara Timur", "Papua",
+                "Papua Barat Daya", "Papua Pegunungan", "Papua Selatan",
+                "Papua Tengah", "Papua Barat", "Riau", "Sulawesi Barat",
+                "Sulawesi Selatan", "Sulawesi Tengah", "Sulawesi Tenggara",
+                "Sulawesi Utara", "Sumatera Barat", "Sumatera Selatan",
+                "Sumatera Utara"
+            };
+
+            if (!IndonesianStates.Contains(state))
+            {
+                return $"State \"{state}\" is not recognized as part of Indonesia.";
+            }
+
             using var jsonDoc = await httpClient.ReadJsonDocAsync($"/alerts/active/area/{state}");
             var jsonElement = jsonDoc.RootElement;
             var alerts = jsonElement.GetProperty("features").EnumerateArray();
@@ -48,6 +68,12 @@ namespace WeatherMCP.Tools
             [Description("The longitude of the location")] double longitude
         )
         {
+            if (latitude < -11 || latitude > 6 || longitude < 95 || longitude > 141)
+            {
+                throw new ArgumentOutOfRangeException(nameof(latitude), "Location is outside INA.");
+            }
+
+
             var pointUri = string.Create(CultureInfo.InvariantCulture, $"/points/{latitude},{longitude}");
             using var jsonDoc = await httpClient.ReadJsonDocAsync(pointUri);
             var forecastUri = jsonDoc.RootElement.GetProperty("properties").GetProperty("forecast").GetString() ?? throw new Exception($"No forecast url provided by {httpClient.BaseAddress}/points/{latitude},{longitude}");
